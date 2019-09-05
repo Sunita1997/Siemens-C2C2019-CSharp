686affcf9adbbbfa8f813e13378c7b67be2329f2
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-05 10:23:44 +0530

    Dya-10 code

---
+++ Day-10/DataAccess_NonSelectQuery/DataAccess_NonSelectQuery/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data; //ConnectionState and CommandType enums
+using System.Data.SqlClient; //SqlConnection, SqlCommand, (SqlDataReader)
+using System.Configuration; //ConfigurationManager
+
+namespace DataAccess_NonSelectQuery
+{
+    class Program
+    {
+        static string GetConnectionString()
+        {
+            string connectionString = null;
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["siemensDbConStr"].ConnectionString;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return connectionString;
+        }
+        static SqlConnection CreateConnection(string connectionString)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return connection;
+        }
+        static SqlCommand CreateCommand(SqlConnection connection, string query, CommandType commandType = CommandType.StoredProcedure)
+        {
+            SqlCommand command = null;
+            try
+            {
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = commandType;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return command;
+        }
+        static void OpenConnection(SqlConnection connection)
+        {           
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        static void CloseConnection(SqlConnection connection)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+        }
+    }
+}
