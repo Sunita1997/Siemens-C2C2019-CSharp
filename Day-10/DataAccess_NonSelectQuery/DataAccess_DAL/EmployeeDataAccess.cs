92d7b254c7b5badea29161c4de2d6763b529dabc
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-05 11:43:20 +0530

    updated day-9 code

---
+++ Day-10/DataAccess_NonSelectQuery/DataAccess_DAL/EmployeeDataAccess.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using static DataAccess_DAL.Utility.DaoUtility;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess_DAL
+{
+    public class EmployeeDataAccess
+    {
+        public int InsertEmployeeRecord(string empName, string empLocation, decimal empSalary, int empDeptId)
+        {
+            string connectionString = null;
+            SqlConnection connection = null;
+            SqlCommand command = null;
+            string procedureName = null;
+            int result = 0;
+            try
+            {
+                connectionString = GetConnectionString();
+                if (connectionString != null && connectionString != string.Empty)
+                {
+                    connection = CreateConnection(connectionString);
+                    if (connection != null)
+                    {
+                        procedureName = GetProcedureName("INSERT_QUERY");
+                        command = CreateCommand(connection, procedureName);
+
+                        if (command != null)
+                        {
+                            SqlParameter pmName = CreateParameter("@name", empName, SqlDbType.VarChar);
+                            SqlParameter pmSalary = CreateParameter("@salary", empSalary, SqlDbType.Decimal);
+                            SqlParameter pmLocation = CreateParameter("@location", empLocation, SqlDbType.VarChar);
+                            SqlParameter pmDeptid = CreateParameter("@deptid", empDeptId, SqlDbType.Int);
+
+                            command.Parameters.Add(pmName);
+                            command.Parameters.Add(pmSalary);
+                            command.Parameters.Add(pmLocation);
+                            command.Parameters.Add(pmDeptid);
+
+                            /*
+                            command.Parameters.AddWithValue("@name", "Dhananjay");
+                            command.Parameters.AddWithValue("@salary", 23000);
+                            command.Parameters.AddWithValue("@location", "Madurai");
+                            command.Parameters.AddWithValue("@deptid", 1);
+                            */
+
+                            OpenConnection(connection);
+                            result = command.ExecuteNonQuery();
+                            //if (result > 0)
+                            //    Console.WriteLine($"{result} record(s) added successfully");
+                            //else
+                            //    Console.WriteLine($"no record(s) added");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null)
+                    CloseConnection(connection);
+            }
+            return result;
+        }
+    }
+}
---
+++ Day-10/DataAccess_NonSelectQuery/DataAccess_DAL/Utility/DaoUtility.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccess_DAL.Utility
+{
+    internal static class DaoUtility
+    {
+        public static string GetConnectionString()
+        {
+            string connectionString = null;
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["siemensDbConStr"].ConnectionString;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return connectionString;
+        }
+        public static SqlConnection CreateConnection(string connectionString)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return connection;
+        }
+        public static SqlCommand CreateCommand(SqlConnection connection, string query, CommandType commandType = CommandType.StoredProcedure)
+        {
+            SqlCommand command = null;
+            try
+            {
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = commandType;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return command;
+        }
+        public static void OpenConnection(SqlConnection connection)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static string GetProcedureName(string key)
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings[key];
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static SqlParameter CreateParameter(string parameterName, object parameterValue, SqlDbType parameterDbType, ParameterDirection parameterDirection = ParameterDirection.Input)
+        {
+            SqlParameter parameter = null;
+            try
+            {
+                parameter = new SqlParameter();
+                parameter.ParameterName = parameterName;
+                parameter.Value = parameterValue;
+                parameter.SqlDbType = parameterDbType;
+                parameter.Direction = parameterDirection;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return parameter;
+        }
+    }
+}
